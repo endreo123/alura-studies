51ecbaab7482324b35a35a3738c2fe7ae758e2dd
endreo123 <endreo.leoni@gmail.com>
2023-05-28 18:41:26 -0300

    first commit

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.scss'
+
+export class Botao extends React.Component {
+    render(): React.ReactNode {
+        return (
+            <button className="botao">
+                Botão
+            </button>
+        )
+    }
+}
---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Botao } from "../Botao";
+import './style.scss'
+
+export class Formulario extends React.Component {
+    render(): React.ReactNode {
+        return (
+            <form className="novaTarefa">
+                <div className="inputContainer">
+                    <label htmlFor="tarefa">
+                        Adicione um novo estudo
+                    </label>
+                    <input
+                    type="text"
+                    name="tarefa"
+                    id="tarefa"
+                    placeholder="O que você quér estudar"
+                    required
+                    >
+                    </input>
+                </div>
+                <div className="inputContainer">
+                    <label htmlFor="tempo">
+                        Tempo
+                    </label>
+                    <input
+                    type="time"
+                    step='1'
+                    name="tempo"
+                    id="tempo"
+                    min="00:00:00"
+                    max="01:30:00"
+                    required
+                    >
+                    </input>
+                </div>
+                <Botao />
+            </form>
+        )
+    }
+}
